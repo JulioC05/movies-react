acf81870e1779903b507a88f2fdd1318f5d0344c
JulioC05 <skjulio05@gmail.com>
2021-08-21 14:20:58 -0500

    first commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import { ImSpinner8 } from 'react-icons/im';
+import styles from './Spinner.module.css';
+
+export function Spinner() {
+    return (
+        <div className={styles.spinner}>
+            <ImSpinner8 className={styles.spinning} size={60} />
+        </div>
+    )
+}
